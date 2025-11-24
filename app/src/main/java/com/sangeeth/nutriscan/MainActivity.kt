a3740f7c4db7b6109ec74fac1919ea7eb85dd6cf
Sangeeth Amirthanathan <42418189+zansangeeth@users.noreply.github.com>
2025-11-24 23:44:42 +0000

    added simple splash animation for checks

--- app/src/main/java/com/sangeeth/nutriscan/MainActivity.kt
+++ app/src/main/java/com/sangeeth/nutriscan/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.sangeeth.nutriscan.ui.theme.ComposeLoadingAnimationsTheme
 import com.sangeeth.nutriscan.ui.theme.NutriScanTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,31 +22,23 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            NutriScanTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            ComposeLoadingAnimationsTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    containerColor = Color.Black
+                ) { innerPadding ->
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        contentAlignment = Alignment.BottomCenter
+                    ) {
+                        BlurredAnimatedText(
+                            text = "NutriScan"
+                        )
+                    }
                 }
             }
         }
-    }
-}
-//main-test
-// dev-branch-test
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NutriScanTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/sangeeth/nutriscan/ui/theme/Theme.kt
+++ app/src/main/java/com/sangeeth/nutriscan/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.sangeeth.nutriscan.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -35,6 +34,29 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun NutriScanTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
+@Composable
+fun ComposeLoadingAnimationsTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
